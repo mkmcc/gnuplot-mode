fbb299e2327b7cdc209986a7e21b68f9f4ef4d20
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-07-17 18:52:09 -0700

    updated installation instructions

--- gnuplot-mode.el
+++ gnuplot-mode.el
@@ -35,10 +35,32 @@
 
 ;;; Configuration:
 
-;; Add the following to your .emacs:
+;; If you installed this via `package.el', you should take advantage
+;; of autoloading.  You can customize features using `defvar' and
+;; `eval-after-load', as illustrated below:
+;;
+;; ;; specify the gnuplot executable (if other than "gnuplot")
+;; (defvar gnuplot-program "/sw/bin/gnuplot")
+;;
+;; ;; set gnuplot arguments (if other than "-persist")
+;; (defvar gnuplot-flags "-persist -pointsize 2")
+;;
+;; ;; if you want, add a mode hook.  e.g., the following turns on
+;; ;; spell-checking for strings and comments and automatically cleans
+;; ;; up whitespace on save.
+;; (eval-after-load 'gnuplot-mode
+;;   '(add-hook 'gnuplot-mode-hook
+;;              (lambda ()
+;;                (flyspell-prog-mode)
+;;                (add-hook 'before-save-hook
+;;                          'whitespace-cleanup nil t))))
+
+;; If you installed this file manually, you probably don't want to
+;; muck around with autoload commands.  Instead, add something like
+;; the following to your .emacs:
 
 ;; (require 'gnuplot-mode)
-
+;;
 ;; ;; specify the gnuplot executable (if other than "gnuplot")
 ;; (setq gnuplot-program "/sw/bin/gnuplot")
 ;;
