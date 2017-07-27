7f988ae3af005b5a16564df9c4c9cd7e6c704bca
Mike McCourt <mkmcc@astro.berkeley.edu>
2017-07-27 13:36:58 -0700

    added GPLv3 license

--- gnuplot-mode.el
+++ gnuplot-mode.el
@@ -16,8 +16,8 @@
 ;; and a command to plot the file.
 
 ;; Some of this code is adapted from a more full-featured version by
-;; Bruce Ravel (available here http://xafs.org/BruceRavel/GnuplotMode;
-;; CC license).
+;; Bruce Ravel (available here https://github.com/bruceravel/gnuplot-mode;
+;; GPLv2).
 
 ;; Thanks to everyone, including Christopher Gilbreth and Ralph Möritz,
 ;; for sending suggestions, improvements, and fixes.
